75571fddd04add5952416779ee7691dafe883dd2
Trent Scholl <TrentScholl@users.noreply.github.com>
2023-12-22 12:56:26 +1000

    Changes

--- kubernetes/talos/apply-clusterconfig.sh
+++ kubernetes/talos/apply-clusterconfig.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.21 -f ./clusterconfig/k8s-cluster-01-k8s-01.trentscholl.com.yaml
+talosctl apply-config -n 10.1.1.21 -f ./clusterconfig/k8s-cluster-01-k8s-node-01.trentscholl.com.yaml
+talosctl apply-config -n 10.1.1.22 -f ./clusterconfig/k8s-cluster-01-k8s-node-02.trentscholl.com.yaml
+talosctl apply-config -n 10.1.1.23 -f ./clusterconfig/k8s-cluster-01-k8s-node-03.trentscholl.com.yaml
